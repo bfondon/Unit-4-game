0e11067506c03cce454ac346d4f0b57567a02846
“Ben <ben.fondon@gmail.com>
2018-11-01 10:04:23 -0500

    Commit with fully functioning JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,49 +9,76 @@ var computerGuess = Math.floor(Math.random()*102+19);
 //Display computer generated number.
 $("#computer-number").text(computerGuess);
 
+//Crystals generate a number
+var crystal1 = Math.floor(Math.random()*11+1);
+var crystal2 = Math.floor(Math.random()*11+1);
+var crystal3 = Math.floor(Math.random()*11+1);
+var crystal4 = Math.floor(Math.random()*11+1);
+
+//Define Reset function.
+function reset(){
+crystal1 = Math.floor(Math.random()*11+1);
+crystal2 = Math.floor(Math.random()*11+1);
+crystal3 = Math.floor(Math.random()*11+1);
+crystal4 = Math.floor(Math.random()*11+1);
+computerGuess = Math.floor(Math.random()*102+19);
+totalScore=0;
+$("#computer-number").text(computerGuess);
+$("#total-score").text(totalScore);
+
+}
+
+//Name win/loss functions.
+
+function lose(){
+    alert("Sorry, you lose!");
+    losses++;
+    reset();
+}
+
+function win(){
+    alert("Congratulations, you win!!!!");
+    wins++;
+    reset();
+}
+
 //When the user selects a crystal, its number is added to the total score
 $("#img1").on("click", function(){
-    var crystal1 = Math.floor(Math.random()*11+1);
     totalScore = crystal1 + totalScore;
     $("#total-score").text(totalScore);
+    if(totalScore>computerGuess){
+        lose();
+    } else if (totalScore===computerGuess) {
+        win();
+    }
 });
 
 $("#img2").on("click", function(){
-    var crystal2 = Math.floor(Math.random()*11+1);
     totalScore = crystal2 + totalScore;
     $("#total-score").text(totalScore);
-
+    if(totalScore>computerGuess){
+        lose();
+    } else if (totalScore===computerGuess) {
+        win();
+    }
 });
 
 $("#img3").on("click", function(){
-    var crystal3 = Math.floor(Math.random()*11+1);
     totalScore = crystal3 + totalScore;
     $("#total-score").text(totalScore);
+    if(totalScore>computerGuess){
+        lose();
+    } else if (totalScore===computerGuess) {
+        win();
+    }
 });
 
 $("#img4").on("click", function(){
-    var crystal4 = Math.floor(Math.random()*11+1);
     totalScore = crystal4 + totalScore;
     $("#total-score").text(totalScore);
-});
-
-//If the total score reaches the number given, the user wins and the game restarts, and a win is added
-if ((totalScore!=0) && (totalScore=computerGuess)){
-    alert("Congratulations, you win!!!!");
-    wins++;
-}
-
-//If the user picks a crystal and goes over the nuber given, the losses goes up by one and the game restarts
-if(totalScore > computerGuess){
-    alert("Sorry, you lose!");
-    losses++;
-    function reset(){
-        totalScore=0;
-        //Tell computer to generate another random number
+    if(totalScore>computerGuess){
+        lose();
+    } else if (totalScore===computerGuess) {
+        win();
     }
-}
-
-//Questions:
-//Why is my second if function not working?
-//How do I refresh my computer generated number
-//How can I clean up my code?
+});
